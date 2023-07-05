001fb19e0aee6d801832fb55ddc229831bb95c9c
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-05 11:14:11 +0530

    Added reader.js which read from path, display which show message with result

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
 const fs = require('fs');
-const { getMaxCalorieElf } = require('./src/max-calorie-finder');
+const {  getMaxCaloriesByElves, getMaxThreeCaloriesByElves } = require('./src/max-calorie-finder');
+const { read } = require('./src/reader');
+const { parseCaloriesByElves } = require('./src/calories-by-elves-parser');
 
 const main = () => {
-  const raw = fs.readFileSync('resource/reindeer-with-calories.txt','utf-8').trim();
-  const raw1 = raw.split('\n\n');
+  const path = process.argv[2];
 
-  const caloriesInString = raw1.map((calorie) => {
-  return (calorie.split('\n'));
-  });
-
-  const caloriesCarriedByElves = caloriesInString.map((calories) => {
-    return calories.map((calorie) => {
-     return parseInt(calorie);
-    })
-  })
-
-  console.log(getMaxCalorieElf(caloriesCarriedByElves));
-
+  read(path, parseCaloriesByElves, getMaxCaloriesByElves);
+  read(path, parseCaloriesByElves, getMaxThreeCaloriesByElves);
 }
 
 main();
-
-// a.map((num) => {
-//   return num.map((num1) => {
-//    return parseInt(num1)
-//   })
-// })
--- src/max-calorie-finder.js
+++ src/max-calorie-finder.js
@@ -6,20 +6,40 @@ const sumOfNumbers = (numbers) => {  //numbers = [4,5,6,7,8]
   return numbers.reduce(sum, 0);
 }
 
-const compareNumbers = (a, b) => {
-  return b - a;
+const compareNumbers = (num1, num2) => {
+  return num2 - num1;
 }
 
-const getMaxCalorieElf = (carriedCaloriesByElves) => {
-  const totalCalories = [];
-
-  carriedCaloriesByElves.forEach((calorieCarriedByElv) => {
-    const currentSumOfCalories = sumOfNumbers(calorieCarriedByElv);
-    totalCalories.push(currentSumOfCalories);
-  });
-
-  const [max1, max2, max3] = totalCalories.sort(compareNumbers).slice(0, 3);
-  return sumOfNumbers([max1, max2, max3]);
+const descendingSorter = (list) => {
+  return list.sort(compareNumbers);
 }
 
-exports.getMaxCalorieElf = getMaxCalorieElf;
+const getTotalCaloriesByElves = (caloriesCarriedByElves) => {
+  return caloriesCarriedByElves.reduce((totalCaloriesByElves, caloriesCarriedByElf) => {
+    totalCaloriesByElves.push(sumOfNumbers(caloriesCarriedByElf));
+    return totalCaloriesByElves;
+  }, []);
+}
+
+const display = (message, result) => {
+  console.log(`${message} ${result}`);
+}
+
+//Part 1
+const getMaxCaloriesByElves = (caloriesCarriedByElves) => {
+  const totalCaloriesByElvesInDescending = getTotalCaloriesByElves(caloriesCarriedByElves);
+  const max = descendingSorter(totalCaloriesByElvesInDescending).slice(0, 1);
+  display('PART 1 - The maximum calorie carried by an elf is', max);
+  // return sumOfNumbers(max);
+}
+
+//Part 2
+const getMaxThreeCaloriesByElves = (caloriesCarriedByElves) => {
+  const totalCaloriesByElvesInDescending = getTotalCaloriesByElves(caloriesCarriedByElves);
+  const [max1, max2, max3] = descendingSorter(totalCaloriesByElvesInDescending).slice(0, 3);
+  display('PART 2 - The top three max calories carried by elves are', [max1, max2, max3]);
+  // return sumOfNumbers([max1, max2, max3]);
+}
+
+exports.getMaxCaloriesByElves = getMaxCaloriesByElves;
+exports.getMaxThreeCaloriesByElves = getMaxThreeCaloriesByElves;
