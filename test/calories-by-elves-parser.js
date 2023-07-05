c089355889adeb635cf76e1d4cc38c584c02a0da
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-05 10:00:24 +0530

    parseCaloriesByElves is functional for multiple elves

--- test/calories-by-elves-parser.js
+++ test/calories-by-elves-parser.js
@@ -11,5 +11,17 @@ describe('parseCaloriesByElves', () => {
     const expected = [[1, 2]];
 
     deepStrictEqual(actual, expected);
+  });
+  it('Should parse and give calorie of multiple elves', () => {
+    const rawText = `1
+    2
+
+    3
+    2
+    `;
+    const actual = parseCaloriesByElves(rawText);
+    const expected = [[1, 2], [3, 2]];
+
+    deepStrictEqual(actual, expected);
   })
 })
