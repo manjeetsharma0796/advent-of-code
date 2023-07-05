f43c229f2e12a6fccd053783ab37b713c8476293
Manjeet Sharma <manjeetsharma0796@users.noreply.github.com>
2023-07-05 12:09:01 +0530

    Removed unwanted files

--- src/calories-by-elves-parser.js
+++
@@ -1,7 +0,0 @@
-const parseCaloriesByElves = (raw) => {
-  return raw.trim().split('\n\n').map((caloriesByElf) => {
-    return caloriesByElf.split('\n').map((numInText) => +numInText)
-  })
-}
-
-exports.parseCaloriesByElves = parseCaloriesByElves;
